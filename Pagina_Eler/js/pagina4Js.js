4232c005e49f4e9a1ad00d1c0f7fbeb0a0ad9bc4
Will224-code <a24216894@alumnos.uady.mx>
2025-06-25 21:51:44 -0600

    update

---
+++ Pagina_Eler/js/pagina4Js.js
@@ -0,0 +1,5 @@
+// js/pagina5Js.js
+document.getElementById('minecraft-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('¡Gracias por responder la encuesta de Minecraft Java!');
+});
